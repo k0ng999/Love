66c38955cc57277f88182c44a484ade648b8eb15
k0ng999 <baydar_14@maul.ru>
2025-05-02 01:32:15 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/loveproject", // 👈 ОБЯЗАТЕЛЬНО укажи название репозитория с косой чертой
   plugins: [react()],
-})
+});
